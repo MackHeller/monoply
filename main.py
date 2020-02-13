f6092fc80bbeeba03795564dc06aa0d26d678cea
Mack Heller <mack.heller@closingfolders.com>
2020-02-13 15:38:17 -0500

    [FEAT] move players

--- main.py
+++ main.py
@@ -13,19 +13,39 @@ class MonopolyGame():
 
 class Board():
 
-    def __init__(self):
-        self._build_board()
+    def __init__(self, board_size=40):
+        self._build_board(board_size)
         self._positions = {}
 
-    def _build_board(self):
-        self.squares = [Square() for _ in range(40)]
-        self.start_square = self.squares[0]
+    @property
+    def board_size(self):
+        return len(self._squares)
+
+    def _build_board(self, board_size):
+        self._squares = [Square(i) for i in range(board_size)]
+        self.start_square = self._squares[0]
 
     def add_player(self, player):
         self._positions[player] = self.start_square
 
     def get_player_position(self, player):
         return self._positions[player]
+
+    def advance_player(self, player, amount):
+        current_square = self.get_player_position(player)
+        index = self._squares.index(current_square)
+        new_index = (index + amount) % self.board_size
+        new_square = self._squares[new_index]
+        self.move_player_to_square(player, new_square)
+
+    def move_player_to_square(self, player, square):
+        self._positions[player] = square
+
+    def get_square_by_name(self, name):
+        for s in self._squares:
+            if s.name == name:
+                return s
+        assert False, 'Square {} does not exist on this board'.format(name)
 
 
 class Player():
@@ -36,8 +56,12 @@ class Player():
 
 class Square():
 
-    def __init__(self):
-        pass
+    def __init__(self, id):
+        self.id = id
+
+    @property
+    def name(self):
+        return str(self.id)
 
 
 if __name__ == '__main__':
--- test_game.py
+++ test_game.py
@@ -9,3 +9,21 @@ def test_init_many_players():
     game.add_players(players)
 
     assert all(board.get_player_position(p) == board.start_square for p in players)
+
+
+def test_advance_player():
+    board = Board()
+    player = Player()
+    board.add_player(player)
+    assert board.get_player_position(player) == board.start_square
+    board.advance_player(player, 4)
+    assert board.get_player_position(player) == board.get_square_by_name('4')
+
+
+def test_move_player():
+    board = Board()
+    player = Player()
+    board.add_player(player)
+    assert board.get_player_position(player) == board.start_square
+    board.move_player_to_square(player, board.get_square_by_name('10'))
+    assert board.get_player_position(player) == board.get_square_by_name('10')
